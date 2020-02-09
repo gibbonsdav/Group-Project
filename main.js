0b30d6ab45ee5482275e16ec43102885f68fcbc5
David Gibbons <gibbonsdav@gmail.com>
2020-02-09 14:47:12 -0800

    added photos

--- main.js
+++ main.js
@@ -13,18 +13,20 @@ $(document).ready(function(e) {
   ) {
     var apps = data.appetizers.map(function(item) {
       return `
-    <div class="menu3>
-    <p class="menutest">${item.name}</p>
-    <p class="menutest">${item.price}</p>
+    <div class="menu3">
+    <h1>Appetizers</h1>
+    <h3 class="menutest">${item.name}</h3>
+    <h3 class="menutest">${item.price}</h3>
     <p class="menutest">${item.description}</p>
     </div>
     `
     })
     var entree = data.entrees.map(function(item) {
       return `
-  <div class="menu4>
-  <p class="menuentree">${item.name}</p>
-  <p class="menuentree">${item.price}</p>
+  <div class="menu4">
+  <h1>Entrees</h1>
+  <h3 class="menuentree">${item.name}</h3>
+  <h3 class="menuentree">${item.price}</h3>
   <p class="menuentree">${item.description}</p>
   </div>
   `
@@ -33,18 +35,20 @@ $(document).ready(function(e) {
     var desserts = data.desserts
       .map(function(item) {
         return `
-  <div class="menu4>
-  <p class="menuentree">${item.name}</p>
-  <p class="menuentree">${item.price}</p>
+  <div class="menu5">
+  <h1>Desserts</h1>
+  <h3 class="menuentree">${item.name}</h3>
+  <h3 class="menuentree">${item.price}</h3>
   <p class="menuentree">${item.description}</p>
   </div>
   `
       })
       // Below, if I send the apps, entrees, and desserts to different divs they will show up on the page but they will cover up stuff they shouldnt like the reservation and story content. If I send them to the same div as below, only the last one called will show up at all
+
       .join("\n")
-    $("#menudisplay").html(apps)
-    $("#menudisplay").html(entree)
-    $("#menudisplay").html(desserts)
+    $("#menu3").html(apps)
+    $("#menu4").html(entree)
+    $("#menu5").html(desserts)
   })
 
   //   .get("https://obscure-tundra-54269.herokuapp.com/casual-dining").data()
