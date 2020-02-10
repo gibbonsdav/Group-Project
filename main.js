8c433557d95ca3e6b1b055ceaaa123b6b6b9d5fe
David Gibbons <gibbonsdav@gmail.com>
2020-02-09 16:34:49 -0800

    finished?

--- main.js
+++ main.js
@@ -48,6 +48,22 @@ $(document).ready(function(e) {
     $("#menu5").html(desserts)
   })
 
+  var slideIndex = 0
+  showSlides()
+  function showSlides() {
+    var i
+    var slides = document.getElementsByClassName("Specials")
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"
+    }
+    slideIndex++
+    if (slideIndex > slides.length) {
+      slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block"
+    setTimeout(showSlides, 86400000)
+  }
+
   //   .get("https://obscure-tundra-54269.herokuapp.com/casual-dining").data()
 
   //   const html = items.results
